13b3a30534a7d9883f12a04f94554675e0db4329
TheGoldenChicken <karlmeisner99@gmail.com>
2022-06-02 14:01:55 +0200

    add split reward to sh files and experiment py

--- gym/specific_experiment_class.py
+++ gym/specific_experiment_class.py
@@ -16,45 +16,90 @@ from decision_transformer.training.seq_trainer import SequenceTrainer
 
 from tqdm import tqdm
 
+
 def discount_cumsum(x, gamma):
     discount_cumsum = np.zeros_like(x)
     discount_cumsum[-1] = x[-1]
-    for t in reversed(range(x.shape[0]-1)): # Why do this reversed?? No idea
+    for t in reversed(range(x.shape[0] - 1)):  # Why do this reversed?? No idea
         # Because they need the discount cumsum of t+1, u dounce!
-        discount_cumsum[t] = x[t] + gamma * discount_cumsum[t+1]
+        discount_cumsum[t] = x[t] + gamma * discount_cumsum[t + 1]
     return discount_cumsum
+
+
+class experiment():
+    def __init__(self, env_name):
+        self.device = 'gpu' if torch.cuda.is_avaliable() else 'cpu'
+        self.reward_scaling = 1000 # Default for DT paper
+        self.env_name = env_name
+
+    def load_data(self, dataset):
+        dataset_path = f'data/{self.env_name}-{dataset}-v2.pkl'
+        with open(dataset_path, 'rb') as f:
+            trajectories = pickle.load(f)
+
+        states, traj_lens, returns = [], [], []
+
+        print('Creating states list')
+        for path in tqdm(trajectories):
+            states.append(path['observations'])
+            traj_lens.append(len(path['observations']))
+            returns.append(path['rewards'].sum())
+        traj_lens, returns = np.array(traj_lens), np.array(returns)
+
+        states = np.concatenate(states, axis=0)  # Flattens states along axis 0? # Check this code in debugger
+        state_mean, state_std = np.mean(states, axis=0), np.std(states, axis=0) + 1e-6
+        # state_dim number of means and stds
+
+        num_timesteps = sum(traj_lens)
+
+        print('=' * 50)
+        print(f'Loaded dataset: {self.env_name} {dataset}')
+        print(f'{len(traj_lens)} trajectories, {num_timesteps} timesteps found')
+        print(f'Average return: {np.mean(returns):.2f}, std: {np.std(returns):.2f}')
+        print(f'Max return: {np.max(returns):.2f}, min: {np.min(returns):.2f}')
+        print('=' * 50)
+
+        p_sample = traj_lens / sum(traj_lens)
+
+
+    def run_experiment(self, dataset, reward_targets, max_ep_len=1000):
+        env = gym.make(self.env_name)
+        state_dim = env.observation_space.shape[0]
+        act_dim = env.action_space.shape[0]
 
 
 def experiment(
         exp_prefix,
         variant,
 ):
-    device = variant.get('device', 'cpu')
-    log_to_wandb = variant.get('log_to_wandb', False)
+    device = 'gpu' if torch.cuda.is_avaliable() else 'cpu'
+    log_to_wandb = True
+
+
 
     env_name, dataset = variant['env'], variant['dataset']
     group_name = f'{exp_prefix}-{env_name}-{dataset}'
     pseudo_unique = random.randint(int(1e5), int(1e6) - 1)
     exp_prefix = f'{group_name}-{pseudo_unique}'
 
-################## DEFINE ENV ##################
+    ################## DEFINE ENV ##################
 
     if env_name == 'hopper':
         env = gym.make('Hopper-v3')
         max_ep_len = 1000
-        env_targets = np.arange(1800, 3600+100, 100)
+        env_targets = np.arange(1800, 3600 + 100, 100)
         # env_targets = [3600, 1800]  # evaluation conditioning targets
         scale = 1000.  # normalization for rewards/returns
     elif env_name == 'halfcheetah':
         env = gym.make('HalfCheetah-v3')
         max_ep_len = 1000
-        env_targets = np.arange(6000, 12000+400, 400)
+        env_targets = np.arange(6000, 12000 + 400, 400)
         # env_targets = [12000, 6000]
         scale = 1000.
     elif env_name == 'walker2d':
         env = gym.make('Walker2d-v3')
         max_ep_len = 1000
-        env_targets = np.arange(2500, 5100+200, 200)
+        env_targets = np.arange(2500, 5100 + 200, 200)
         # env_targets = [5000, 2500]
         scale = 1000.
     else:
@@ -64,11 +109,7 @@ def experiment(
     act_dim = env.action_space.shape[0]
 
     # load dataset
-    if variant['split_data'] == False:
-        dataset_path = f'data/{env_name}-{dataset}-v2.pkl'
-    else:
-        dataset_path = f'data/{env_name}-{dataset}-split-reward-v2.pkl'
-
+    dataset_path = f'data/{env_name}-{dataset}-v2.pkl'
     with open(dataset_path, 'rb') as f:
         trajectories = pickle.load(f)
         # trajectories is a list of dicts
@@ -89,7 +130,7 @@ def experiment(
     # Returns are end reward for all trajectories
 
     # used for input normalization
-    states = np.concatenate(states, axis=0) # Flattens states along axis 0? # Check this code in debugger
+    states = np.concatenate(states, axis=0)  # Flattens states along axis 0? # Check this code in debugger
     state_mean, state_std = np.mean(states, axis=0), np.std(states, axis=0) + 1e-6
     # state_dim number of means and stds
 
@@ -119,12 +160,13 @@ def experiment(
             # So this is a bit weird, because the way we create p_sample from traj_lens[sorted_inds], we are actually
             # sampling based on final return. That is trajectories[sorted_inds[900]] will have lower reward than if batch_inds = 1000...
             traj = trajectories[batch_inds[i]]
-            si = random.randint(0, traj['rewards'].shape[0] - 1) # Random number between 0 and len of trajectory - 1
+            si = random.randint(0, traj['rewards'].shape[0] - 1)  # Random number between 0 and len of trajectory - 1
 
             # get sequences from dataset
             # Based on context length
             s.append(traj['observations'][si:si + max_len].reshape(1, -1, state_dim))
-            a.append(traj['actions'][si:si + max_len].reshape(1, -1, act_dim)) # Just reshapes to have 'extra dimension'
+            a.append(
+                traj['actions'][si:si + max_len].reshape(1, -1, act_dim))  # Just reshapes to have 'extra dimension'
             r.append(traj['rewards'][si:si + max_len].reshape(1, -1, 1))
             # Not really sure about this? Probably some envs call them 'dones' rather than terminals... stupid, maybe gym vs mujoco?
             if 'terminals' in traj:
@@ -132,9 +174,12 @@ def experiment(
             else:
                 d.append(traj['dones'][si:si + max_len].reshape(1, -1))
             timesteps.append(np.arange(si, si + s[-1].shape[1]).reshape(1, -1))
-            timesteps[-1][timesteps[-1] >= max_ep_len] = max_ep_len-1  # padding cutoff # remove stuff that is longer than max_ep_len allows
-            rtg.append(discount_cumsum(traj['rewards'][si:], gamma=1.)[:s[-1].shape[1] + 1].reshape(1, -1, 1)) # Only get reward to go in context length (+1 context length for some reason...)
-            if rtg[-1].shape[1] <= s[-1].shape[1]: # Some shape correction here.. don't know when states would ever be longer than reward to go
+            timesteps[-1][timesteps[
+                              -1] >= max_ep_len] = max_ep_len - 1  # padding cutoff # remove stuff that is longer than max_ep_len allows
+            rtg.append(discount_cumsum(traj['rewards'][si:], gamma=1.)[:s[-1].shape[1] + 1].reshape(1, -1,
+                                                                                                    1))  # Only get reward to go in context length (+1 context length for some reason...)
+            if rtg[-1].shape[1] <= s[-1].shape[
+                1]:  # Some shape correction here.. don't know when states would ever be longer than reward to go
                 rtg[-1] = np.concatenate([rtg[-1], np.zeros((1, 1, 1))], axis=1)
 
             # padding and state + reward normalization
@@ -170,7 +215,7 @@ def experiment(
                         model,
                         max_ep_len=max_ep_len,
                         scale=scale,
-                        target_return=target_rew/scale,
+                        target_return=target_rew / scale,
                         mode=variant.get('mode', 'normal'),
                         state_mean=state_mean,
                         state_std=state_std,
@@ -187,6 +232,7 @@ def experiment(
                 # f'target_{target_rew}_length_mean': np.mean(lengths),
                 # f'target_{target_rew}_length_std': np.std(lengths),
             }
+
         return fn
 
     if variant['model_name'] != '':
@@ -200,18 +246,18 @@ def experiment(
 
     else:
         model_kwargs = {
-            'state_dim'           : state_dim,
-            'act_dim'             : act_dim,
-            'max_length'          : variant['K'],
-            'max_ep_len'          : max_ep_len,
-            'hidden_size'         : variant['embed_dim'],
-            'n_layer'             : variant['n_layer'],
-            'n_head'              : variant['n_head'],
-            'n_inner'             : 4*variant['embed_dim'],
-            'activation_function' : variant['activation_function'],
-            'n_positions'         : 1024,
-            'resid_pdrop'         : variant['dropout'],
-            'attn_pdrop'          : variant['dropout']
+            'state_dim': state_dim,
+            'act_dim': act_dim,
+            'max_length': variant['K'],
+            'max_ep_len': max_ep_len,
+            'hidden_size': variant['embed_dim'],
+            'n_layer': variant['n_layer'],
+            'n_head': variant['n_head'],
+            'n_inner': 4 * variant['embed_dim'],
+            'activation_function': variant['activation_function'],
+            'n_positions': 1024,
+            'resid_pdrop': variant['dropout'],
+            'attn_pdrop': variant['dropout']
         }
 
         model = DecisionTransformer(**model_kwargs)
@@ -226,16 +272,16 @@ def experiment(
     )
     scheduler = torch.optim.lr_scheduler.LambdaLR(
         optimizer,
-        lambda steps: min((steps+1)/warmup_steps, 1)
+        lambda steps: min((steps + 1) / warmup_steps, 1)
     )
 
     trainer = SequenceTrainer(
         model=model,
         optimizer=optimizer,
-        batch_size= variant['batch_size'],
+        batch_size=variant['batch_size'],
         get_batch=get_batch,
         scheduler=scheduler,
-        loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: torch.mean((a_hat - a)**2),
+        loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: torch.mean((a_hat - a) ** 2),
         eval_fns=[eval_episodes(tar) for tar in env_targets],
     )
 
@@ -247,7 +293,6 @@ def experiment(
             config=variant
         )
         # wandb.watch(model)  # wandb has some bug
-
 
     save_iters = set(map(int, variant['save_iters'].split(','))) if variant['save_iters'] != '' else []
     eval_iters = set(map(int, variant['eval_iters'].split(','))) if variant['eval_iters'] != '' else []
@@ -265,7 +310,6 @@ def experiment(
             file.close()
 
             torch.save(trainer.model.state_dict(), path + '-model')
-
 
         if iter in eval_iters:
             eval_outputs = trainer.evaluate(num_steps=variant['num_steps_per_iter'], iter_num=iter, print_logs=True)
@@ -291,10 +335,10 @@ if __name__ == '__main__':
 
     parser.add_argument('--model_name', type=str, default='')
 
-    parser.add_argument('--save_iters', type=str, default='') # string like '5,10,15'
+    parser.add_argument('--save_iters', type=str, default='')  # string like '5,10,15'
     parser.add_argument('--save_path', type=str, default='./saved_models/')
-    
-    parser.add_argument('--eval_iters', type=str, default='1') # string like '5,10,15'
+
+    parser.add_argument('--eval_iters', type=str, default='1')  # string like '5,10,15'
 
     parser.add_argument('--env', type=str, default='hopper')
     parser.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
@@ -303,13 +347,12 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, default='cpu')
     parser.add_argument('--log_to_wandb', '-w', type=bool, default=False)
 
-
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--embed_dim', type=int, default=128)
 
     parser.add_argument('--num_steps_per_iter', type=int, default=10000)
-    
-    parser.add_argument('--K', type=int, default=20) # contect window
+
+    parser.add_argument('--K', type=int, default=20)  # contect window
     parser.add_argument('--n_layer', type=int, default=3)
     parser.add_argument('--n_head', type=int, default=1)
     parser.add_argument('--activation_function', type=str, default='relu')
@@ -318,8 +361,6 @@ if __name__ == '__main__':
     parser.add_argument('--weight_decay', '-wd', type=float, default=1e-4)
     parser.add_argument('--warmup_steps', type=int, default=10000)
     parser.add_argument('--mode', type=str, default='normal')
-
-    parser.add_argument('split_reward', type=bool, default=False)
 
     args = parser.parse_args()
 
