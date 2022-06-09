d97e1393991a41037d67733227a8d3eee7be23ff
Christian Nielsen <nielsenchristianole@gmail.com>
2022-06-09 09:16:11 +0200

    did some more experiments and the first multireward trail

--- gym/sh_submissions/submit_walker.sh
+++ gym/sh_submissions/submit_walker.sh
@@ -7,7 +7,7 @@
 #BSUB -n 4
 #BSUB -R "rusage[mem=4G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 15:00
+#BSUB -W 20:00
 # end of BSUB options
 
 
@@ -19,4 +19,4 @@ source $HOME/miniconda3/envs/decision-transformer-gym/bin/activate
 wandb on
 echo 'cad8b043f3731a2c453efd8f61915e186ac93ac3' | wandb login
 
-python experiment_single_reward.py --env 'walker2d' --dataset 'medium_replay' --seed 42  --save_iters '8,10,12' --eval_iters '8,9,10,11,12' --max_iters 12 --device 'cuda' --log_to_wandb True
+python experiment_single_reward.py --env 'walker2d' --dataset 'medium_replay' --seed 46  --save_iters '8,10,12' --eval_iters '8,9,10,11,12' --max_iters 12 --device 'cuda' --log_to_wandb True
